af80d5fa35353e745afa2905df1b2ae7362ab316
Terminator <lastmonopoly@outlook.com>
2022-04-17 16:49:56 +0800

    Add Chinese translation

--- vignettes/automotive/lib/main.dart
+++ vignettes/automotive/lib/main.dart
@@ -16,45 +16,62 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  List<String> demos = const ['Wallpaper', 'Statistics', 'Music', 'Shopping'];
+  List<String> demos = const ['Wallpaper', 'Statistics', 'Music', 'Shopping', 'Disclaimer'];
   String mode = 'Wallpaper';
   Color themeColor = Color(0xFFD96C00);
 
   Widget _buildSelector() {
     return Container(
       color: Colors.black,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          for (String m in demos)
-            GestureDetector(
-              onTap: () {
-                setState(() {
-                  mode = m;
-                });
-              },
-              child: Center(
-                child: Text(
-                  m,
-                  style: TextStyle(
-                    fontSize: 17,
-                    fontFamily: 'Unica',
-                    color: m == mode ? themeColor : Colors.white,
-                    fontWeight: m == mode ? FontWeight.w600 : FontWeight.w400,
-                  ),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            for (String m in demos)
+              GestureDetector(
+                onTap: () {
+                  setState(() {
+                    mode = m;
+                  });
+                },
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      _translate(m),
+                      style: TextStyle(
+                        fontSize: 15,
+                        letterSpacing: .5,
+                        color: m == mode ? themeColor : Colors.white,
+                        fontWeight: m == mode ? FontWeight.w600 : FontWeight.w400,
+                      ),
+                    ),
+                    SizedBox(height: 8),
+                    Text(
+                      m,
+                      style: TextStyle(
+                        fontSize: 17,
+                        fontFamily: 'Unica',
+                        color: m == mode ? themeColor : Colors.white,
+                        fontWeight: m == mode ? FontWeight.w600 : FontWeight.w400,
+                      ),
+                    ),
+                  ],
                 ),
               ),
-            ),
-        ],
+          ],
+        ),
       ),
     );
   }
 
-  String _translate() {
+  String _translate(String mode) {
     if (mode == 'Statistics') return '统计';
     if (mode == 'Wallpaper') return '壁纸';
     if (mode == 'Music') return '音乐';
-    return '购物';
+    if (mode == 'Shopping') return '购物';
+    return '声明';
   }
 
   Widget _buildDemo() {
